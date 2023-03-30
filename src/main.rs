0b52866493216e2f152447b82b8d889bc889fa35
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-03-30 09:15:46 +0530

    getting day of the week

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+enum Day {
+    Mon, Tue, Wed, Thu, Fri, Sat, Sun,
+}
+
+fn get_todays_day() -> Day {
+    use std::time::SystemTime;
+
+    let day_of_week: u64;
+
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => {
+            let sec = n.as_secs();
+            let days_since_epoch = sec / (24 * 60 * 60);
+            day_of_week = days_since_epoch % 7
+        }
+        Err(e) => {
+            eprintln!("Failed getting system time due to: {e}");
+            std::process::exit(1);
+        }
+    };
+
+    // UNIX_EPOCH i.e. Jan 1, 1970 was Thu which means 0 will be Thu
+    match day_of_week {
+        0 => Day::Thu,
+        1 => Day::Fri,
+        2 => Day::Sat,
+        3 => Day::Sun,
+        4 => Day::Mon,
+        5 => Day::Tue,
+        6 => Day::Wed,
+        _ => unreachable!("Wrong day of the week, this should never be the case unless my
+                          calculations are wrong"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
 }
